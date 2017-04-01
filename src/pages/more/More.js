318b0c6e5911f2339f61121cdfd3a1bbee53385e
Eugene Koval <iam.eugenekoval@gmail.com>
2017-04-01 18:34:59 +0300

    update

--- src/pages/more/More.js
+++ src/pages/more/More.js
@@ -20,52 +20,50 @@ class More extends Component {
     return(
       <div>
         <Header />
-        <div className="main-wrapper">
-          <Menu />
+        <Menu />
 
-          <div className="main-container">
-            <div className="main-row clear">
-              <div className="main-content">
+        <div className="main-container">
+          <div className="main-row clear">
+            <div className="main-content">
 
-                <div className="card">
-                  <div className="card__content">
-                    <div className="card__title">{userMore.name} {userMore.surname}</div>
-                    <div className="card__subtitle">Специализация: {userMore.specialization}</div>
-                    <div className="card__description">{userMore.description}</div>
-                    <div>{userMore.addres}</div>
-                    <ul className="card__list">
-                      {userMore.options.map(function(list, index){
-                        return(<li className="card__list-item" key={index}>{list}</li>)
-                      })}
-                    </ul>
-                    <div className="star-rating">
-                      <Rating />
-                    </div>
-                    <div className="service__comment">
-                      <a href="#">Отзывы: { userMore.comment } </a>
-                    </div>
-                    <div className="service__button">
-                      <Link to="" className="btn btn--ghost">Заказать услугу</Link>
-                      <Link to="/" className="btn btn--ghost">Назад</Link>
-                    </div>
+              <div className="card">
+                <div className="card__content">
+                  <div className="card__title">{userMore.name} {userMore.surname}</div>
+                  <div className="card__subtitle">Специализация: {userMore.specialization}</div>
+                  <div className="card__description">{userMore.description}</div>
+                  <div>{userMore.addres}</div>
+                  <ul className="card__list">
+                    {userMore.options.map(function(list, index){
+                      return(<li className="card__list-item" key={index}>{list}</li>)
+                    })}
+                  </ul>
+                  <div className="star-rating">
+                    <Rating />
+                  </div>
+                  <div className="service__comment">
+                    <a href="#">Отзывы: { userMore.comment } </a>
+                  </div>
+                  <div className="service__button">
+                    <Link to="" className="btn btn--ghost">Заказать услугу</Link>
+                    <Link to="/" className="btn btn--ghost">Назад</Link>
                   </div>
                 </div>
+              </div>
 
-              </div>
-              <div className="main-sidebar">
-                <Sidebar />
-              </div>
+            </div>
+            <div className="main-sidebar">
+              <Sidebar />
             </div>
           </div>
-
-            <div className="service more">
-              <div className="service__wrap-avatar">
-                <img src={img3}/>
-              </div>
-            </div>
-
-          <Footer />
         </div>
+
+        <div className="service more">
+          <div className="service__wrap-avatar">
+            <img src={img3}/>
+          </div>
+        </div>
+
+        <Footer />
       </div>
     );
   }
